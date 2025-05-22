8a9b33a4a83d128703a7a1459ffacd8ec477c25c
Thanh Binh <binh.tran-thanh@banvien.com.vn>
2025-05-22 16:17:01 +0700

    Rainbow effect

---
+++ build/src/CMakeFiles/RubiksCube.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/banvien/eclipse-workspace/Loader_3D/src/Application.cpp" "/home/banvien/eclipse-workspace/Loader_3D/build/src/CMakeFiles/RubiksCube.dir/Application.cpp.o"
+  "/home/banvien/eclipse-workspace/Loader_3D/src/Controller.cpp" "/home/banvien/eclipse-workspace/Loader_3D/build/src/CMakeFiles/RubiksCube.dir/Controller.cpp.o"
+  "/home/banvien/eclipse-workspace/Loader_3D/src/Cube.cpp" "/home/banvien/eclipse-workspace/Loader_3D/build/src/CMakeFiles/RubiksCube.dir/Cube.cpp.o"
+  "/home/banvien/eclipse-workspace/Loader_3D/src/Pyramid.cpp" "/home/banvien/eclipse-workspace/Loader_3D/build/src/CMakeFiles/RubiksCube.dir/Pyramid.cpp.o"
+  "/home/banvien/eclipse-workspace/Loader_3D/src/Renderer.cpp" "/home/banvien/eclipse-workspace/Loader_3D/build/src/CMakeFiles/RubiksCube.dir/Renderer.cpp.o"
+  "/home/banvien/eclipse-workspace/Loader_3D/imgui/backends/imgui_impl_glfw.cpp" "/home/banvien/eclipse-workspace/Loader_3D/build/src/CMakeFiles/RubiksCube.dir/__/imgui/backends/imgui_impl_glfw.cpp.o"
+  "/home/banvien/eclipse-workspace/Loader_3D/imgui/backends/imgui_impl_opengl3.cpp" "/home/banvien/eclipse-workspace/Loader_3D/build/src/CMakeFiles/RubiksCube.dir/__/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "/home/banvien/eclipse-workspace/Loader_3D/imgui/imgui.cpp" "/home/banvien/eclipse-workspace/Loader_3D/build/src/CMakeFiles/RubiksCube.dir/__/imgui/imgui.cpp.o"
+  "/home/banvien/eclipse-workspace/Loader_3D/imgui/imgui_demo.cpp" "/home/banvien/eclipse-workspace/Loader_3D/build/src/CMakeFiles/RubiksCube.dir/__/imgui/imgui_demo.cpp.o"
+  "/home/banvien/eclipse-workspace/Loader_3D/imgui/imgui_draw.cpp" "/home/banvien/eclipse-workspace/Loader_3D/build/src/CMakeFiles/RubiksCube.dir/__/imgui/imgui_draw.cpp.o"
+  "/home/banvien/eclipse-workspace/Loader_3D/imgui/imgui_tables.cpp" "/home/banvien/eclipse-workspace/Loader_3D/build/src/CMakeFiles/RubiksCube.dir/__/imgui/imgui_tables.cpp.o"
+  "/home/banvien/eclipse-workspace/Loader_3D/imgui/imgui_widgets.cpp" "/home/banvien/eclipse-workspace/Loader_3D/build/src/CMakeFiles/RubiksCube.dir/__/imgui/imgui_widgets.cpp.o"
+  "/home/banvien/eclipse-workspace/Loader_3D/src/main.cpp" "/home/banvien/eclipse-workspace/Loader_3D/build/src/CMakeFiles/RubiksCube.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/lib/cmake/glm"
+  "../imgui"
+  "../imgui/backends"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/RubiksCube.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RubiksCube.dir/Application.cpp.o"
+  "CMakeFiles/RubiksCube.dir/Controller.cpp.o"
+  "CMakeFiles/RubiksCube.dir/Cube.cpp.o"
+  "CMakeFiles/RubiksCube.dir/Pyramid.cpp.o"
+  "CMakeFiles/RubiksCube.dir/Renderer.cpp.o"
+  "CMakeFiles/RubiksCube.dir/__/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/RubiksCube.dir/__/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/RubiksCube.dir/__/imgui/imgui.cpp.o"
+  "CMakeFiles/RubiksCube.dir/__/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/RubiksCube.dir/__/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/RubiksCube.dir/__/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/RubiksCube.dir/__/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/RubiksCube.dir/main.cpp.o"
+  "RubiksCube"
+  "RubiksCube.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RubiksCube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
